0b49ef65c10585204ef0be22f1865c68642df847
artisann <artisann@artisann.dev>
2025-12-20 01:20:16 -0600

    Artisann was here

---
+++ src/lib/about-me.ts
@@ -0,0 +1,97 @@
+
+type WorkExperience = {
+    logo: string,
+    company: string,
+    role: string,
+    technologies: string[],
+    description: string,
+    start: string,
+    end: string
+}
+
+export const workExperiences: WorkExperience[] = [
+    {
+        logo: '/verizon-logo.png',
+        company: 'Verizon',
+        role: 'Software Engineer - Automation & Device Intelligence',
+        technologies: ['TS', 'Java', 'Python'],
+        description: 'Developed and maintained automation frameworks for device testing, improving efficiency and reducing manual testing efforts. Collaborated with telecom protocol teams to analyze signaling flows (SIP, VoLTE, IMS) identify defects and support fast releases.',
+        start: 'Jan 2025',
+        end: 'Present'
+    },
+    {
+        logo: '/recon-logo.png',
+        company: 'Recon Forensics',
+        role: 'Contractor',
+        technologies: ['OCR', 'Blockchain'],
+        description: 'Supported the Attorney general in complex cases assisting in computer forensics and blockchain analysis.',
+        start: 'Sept 2025',
+        end: 'Present'
+    },
+    {
+        logo: '/perforce-logo.png',
+        company: 'Perforce Software',
+        role: 'Systems Engineer',
+        technologies: ['ADB', 'Mobile Testing'],
+        description: 'Managed global automation labs for perfecto. Supporting high-scale mobile testing environments.',
+        start: 'April 2021',
+        end: 'Jan 2025'
+    }
+]
+
+type PersonalProject = {
+    name: string,
+    description: string,
+    technologies: string[],
+    link?: string,
+    badge?: 'soon' | 'deprecated'
+}
+
+export const personalProjects: PersonalProject[] = [
+    {
+        name: 'Blocky',
+        description: 'Official Notion (notion.so) integration allowing users to create customizable widgets dynamically from their notion data sources.',
+        technologies: ['React','Next.js', 'Postgres', 'Redis', 'Effect-TS'],
+        link: 'https://www.blocky.so'
+    },
+    {
+        name: 'SynqUp (In development)',
+        description: 'Cross-platform app for group planning with auto-synced calendars, social event feeds, and trip coordination features.',
+        technologies: ['React Native', 'Expo', 'Google, Apple, and Outlook calendar APIs'],
+        badge: 'soon'
+    },
+    {
+        name: 'Brigade',
+        description: 'Multi-Wallet crypto trading platform. Executed over 160k+ in user trading volume across 8 decentralized protocols with real-time price feeds',
+        technologies: ['Telegram', 'Solana', 'Node.js', 'Go'],
+        badge: 'deprecated'
+    },
+    {
+        name: 'Twisted Fate',
+        description: 'A fully automated virtual betting game/experience for League of Legends. Served over 10k+ users with persistent rank, rewards systems, and real-time match tracking.',
+        technologies: ['RIOT API', 'Java', 'Discord'],
+        badge: 'deprecated'
+    }
+]
+
+type OpenSourceProject = {
+    name: string,
+    description: string,
+    technologies: string[],
+    link: string
+}
+
+export const openSourceProjects: OpenSourceProject[] = [
+    {
+        name: 'Fillcut',
+        description: 'FillCut is a tool that automatically removes filler words, pauses, and coughs from video files.',
+        technologies: ['TS', 'Whisper', 'FFmpeg'],
+        link: 'https://github.com/ImArtisann/fillcut'
+    },
+    {
+        name: 'Pump.fun Websocket',
+        description: 'Package to easily subscribe to events that take place on pump.fun subscribe to new trades created or to new coins created',
+        technologies: ['JS', 'Socket.IO'],
+        link: 'https://github.com/ImArtisann/Pump.fun-Websocket'
+    }
+]
---
+++ src/routes/index.tsx
@@ -0,0 +1,140 @@
+import {createFileRoute} from '@tanstack/react-router'
+import {Button} from "@/components/ui/button.tsx";
+import {openSourceProjects, personalProjects, workExperiences} from "@/lib/about-me.ts";
+import {Badge} from "@/components/ui/badge.tsx";
+import {Moon, Sun} from "lucide-react";
+import {useTheme} from "@/components/theme-provider.tsx";
+
+export const Route = createFileRoute('/')({ component: App })
+
+function App() {
+
+  const {theme, setTheme} = useTheme()
+
+
+  return (
+    <div className="min-h-screen bg-background">
+      <header className={'mx-auto max-w-4xl px-6 pt-14'}>
+        <div className={'flex flex-row items-center justify-between'}>
+          <div className={'flex flex-col gap-4 items-start'}>
+            <h1 className={'text-6xl font-bold text-foreground'}>
+              Artisann
+            </h1>
+            <div className={'flex flex-row gap-2 items-center'}>
+              <a href={'https://github.com/ImArtisann'} target="_blank" rel="noreferrer" className={'text-md text-foreground hover:underline'}>
+                GitHub
+              </a>
+              <span className={'text-md text-muted-foreground'}>/</span>
+              <a href={'https://x.com/IArtisann'} target="_blank" rel="noreferrer" className={'text-md text-foreground hover:underline'}>
+                Twitter
+              </a>
+            </div>
+          </div>
+          <Button
+            variant={'ghost'} size={'icon'}
+            onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+          >
+            {theme === 'dark' ? (<Sun className={'h-5 w-5'}/>) : (<Moon className={'h-5 w-5'}/>)}
+
+          </Button>
+        </div>
+      </header>
+      <section className={'mx-auto max-w-4xl px-6 pt-14'}>
+        <div className={'flex flex-col gap-4'}>
+          <h2 className={'font-semibold text-muted-foreground'}>
+            Experience
+          </h2>
+          <div className={'flex flex-col w-full gap-4'}>
+            {workExperiences.map((experience, index) => (
+                <div key={`${experience.company}-${index}`} className={'group flex flex-col w-full gap-2'}>
+                  <div  className={'flex flex-row w-full items-center justify-between'}>
+                    <div className={'flex flex-row items-center gap-4'}>
+                      <img src={experience.logo} alt={'My Cat Milo'} className={'h-6 w-auto'}/>
+                    <div className={'flex flex-col gap-1 items-start'}>
+                      <h3 className={'font-semibold text-foreground'}>
+                        {experience.company}
+                      </h3>
+                      <div className={'flex flex-row gap-2 items-start'}>
+                        <span className={'text-sm text-foreground/80'}>
+                        {experience.role}
+                        </span>
+                        <span className={'text-sm text-muted-foreground'}>
+                          {experience.technologies.join(', ')}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={'flex flex-row gap-2 items-center text-sm text-muted-foreground'}>
+                    <p>{experience.start}</p>
+                    <p>-</p>
+                    <p>{experience.end}</p>
+                  </div>
+                </div>
+                  <div
+                      className={'max-h-0 opacity-0 group-hover:max-h-32 group-hover:opacity-100 transition-all duration-300 ease-in-out overflow-hidden'}>
+                    <p className={'text-sm text-foreground'}>
+                      {experience.description}
+                    </p>
+                  </div>
+                </div>
+            ))}
+          </div>
+        </div>
+      </section>
+      <section className={'mx-auto max-w-4xl px-6 pt-14'}>
+        <div className={'flex flex-col gap-4'}>
+          <h2 className={'font-semibold text-muted-foreground'}>
+            Apps
+          </h2>
+          <div className={'flex flex-col gap-4'}>
+            {personalProjects.map((app, index) => (
+                <div key={`${app.name}-${index}`} className={'flex flex-row w-full items-center justify-start gap-4'}>
+                  <div className={'flex flex-col w-full items-start gap-2'}>
+                    <div className={'flex flex-row text-sm  w-full items-center justify-between'}>
+                    <div className={'flex flex-row text-sm gap-2 items-center'}>
+                      <span className={'font-semibold text-lg text-foreground'}>{app.name}</span>
+                      {app.link && ( <a href={app.link} target="_blank" rel="noreferrer" className={'text-sm text-muted-foreground underline'}>Link</a>)}
+                    </div>
+                      {app.badge && <Badge variant={app.badge === 'soon' ? 'default' : 'destructive'} >{app.badge === 'soon' ? 'Soon' : 'Not Supported'}</Badge> }
+                    </div>
+                    <p className={'text-sm text-foreground/80'}>{app.description}</p>
+                    <span className={'text-sm text-muted-foreground'}>
+                          {app.technologies.join(', ')}
+                        </span>
+                  </div>
+                </div>
+            ))}
+          </div>
+        </div>
+      </section>
+      <section className={'mx-auto max-w-4xl px-6 pt-14'}>
+        <div className={'flex flex-col gap-4'}>
+          <h2 className={'font-semibold text-muted-foreground'}>
+            Open Source
+          </h2>
+          <div className={'flex flex-col gap-4'}>
+            {openSourceProjects.map((app, index) => (
+                <div key={`${app.name}-${index}`} className={'flex flex-row w-full items-center justify-start gap-4'}>
+                  <div className={'flex flex-col w-full items-start gap-2'}>
+                    <div className={'flex flex-row text-sm  w-full items-center justify-between'}>
+                      <div className={'flex flex-row text-sm gap-2 items-center'}>
+                        <span className={'font-semibold text-lg text-foreground'}>{app.name}</span>
+                      </div>
+                      <a href={app.link} target="_blank" rel="noreferrer" className={'text-sm text-muted-foreground underline'}>Link</a>
+                    </div>
+                    <p className={'text-sm text-foreground/80'}>{app.description}</p>
+                    <span className={'text-sm text-muted-foreground'}>
+                          {app.technologies.join(', ')}
+                        </span>
+                  </div>
+                </div>
+            ))}
+          </div>
+        </div>
+      </section>
+      <div className={'absolute bottom-6 left-6'}>
+        <img src={'/Milo.png'} alt={'My Cat Milo'} className={'h-56 w-auto'}/>
+      </div>
+    </div>
+  )
+}
